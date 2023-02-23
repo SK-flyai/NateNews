30fd7b4470723069a77952411b95ee096b403c23
Euidong Yang <yod1073@gmail.com>
2023-02-23 12:51:19 +0900

    FIX: Specify `user_words_path`

--- crawl/nate.py
+++ crawl/nate.py
@@ -7,7 +7,6 @@ import sys
 # ML모듈 내의 predict에 접근하기 위해 경로 수정(접근성을 위해 상대경로를 이용)
 path = 'C:/Users/013/Desktop/code/NateNews/ml'
 sys.path.append(path)
-print(path)
 
 from newspred import NewsModel
 from bareunpy import Tagger
@@ -15,7 +14,11 @@ from bareunpy import Tagger
 API_KEY = 'koba-6REWGFY-OPWE7ZI-TPNV3FQ-DCVQDKA' # 각자의 API키로 변경해주세요!
 
 tagger = Tagger(API_KEY)
-model = NewsModel(tagger=tagger, model_path='sinjy1203/ko-sbert-natenews')
+model = NewsModel(
+    tagger=tagger, 
+    model_path='sinjy1203/ko-sbert-natenews', 
+    user_words_path='C:/Users/013/Desktop/code/NateNews/ml/user_words',
+)
 
 
 def news_keyword(doc: str, title: str):
