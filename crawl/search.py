bc43b1696fdef88912c6fb7c15c43f6bcd00101c
Euidong Yang <yod1073@gmail.com>
2023-02-24 05:09:30 +0900

    Style: Edit crawling directly

--- crawl/search.py
+++ crawl/search.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as bs
+import re
 import requests
 
 URL = "https://news.nate.com/search?q="
@@ -9,10 +10,50 @@ def search_news(keyword: str, num: int=5):
     req = requests.get(url, headers=headers)
     html = bs(req.text, 'html.parser')
     
-    articles = html.find('ul', {'class': 'search-list'})
-    search_list = articles.find_all('a', {'class': 'thumb-wrap'})
-    url_list = [f"https:{article['href']}" for article in search_list]
-    return url_list[:num]
+    articles = html.find_all('li', {'class': 'items'})
+    
+    news_list = list(map(_info, articles[:num]))
+    
+    tmp_dict = dict()
+    for news in news_list:
+        tmp_dict.update(news)
+    
+    return {keyword: tmp_dict}
+
+def _info(article) -> dict:
+    # url
+    url = f"http:{article.find('a').get('href')}"
+    
+    # title
+    title = article.find('span', {'class': 'tit'}).text
+    
+    # content
+    _content = article.find('span', {'class': 'txt'}).text
+    _content = re.sub('[\n\t\r]+', ' ', _content)
+    content = re.sub(' +', ' ', _content)
+    
+    # time and press
+    date_press = article.find('span', {'class': 'time'}).text
+    tmp = re.sub('[\n\t\r]+', ' ', date_press)
+    tmp = re.sub(' +', ' ', tmp)
+    press, date = tmp[:-18], tmp[-17:-1]
+    
+    # image
+    try:
+        image = f"http:{article.find('img').get('src')}"
+    except:
+        # 야매..일단은 이미지 없으면 아무 이미지나..!
+        image = "http://thumbnews.nateimg.co.kr/news200x200///news.nateimg.co.kr/orgImg/na/2023/02/23/5850168_high.jpg"
+    
+    return {
+        url: {
+            "title": title,
+            "img": image,
+            "content": content,
+            "press": press,
+            "date": date,
+        }
+    }
 
 
 if __name__ == "__main__":
